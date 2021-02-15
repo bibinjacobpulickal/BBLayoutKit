0cb8ab943758f507b5d3fccc032316d34f200580
Bibin Jacob Pulickal <bibinjacob123@gmail.com>
2021-02-15 20:34:40 +0530

    BBLayoutKitTests: Added testAddSubview.

--- Tests/BBLayoutKitTests/BBLayoutKitTests.swift
+++ Tests/BBLayoutKitTests/BBLayoutKitTests.swift
@@ -2,14 +2,25 @@ import XCTest
 @testable import BBLayoutKit
 
 final class BBLayoutKitTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual("BBLayoutKit", "BBLayoutKit")
-    }
 
     static var allTests = [
         ("testExample", testExample),
+        ("testAddSubview", testAddSubview)
     ]
+
+    func testExample() {
+        XCTAssertEqual("BBLayoutKit", "BBLayoutKit")
+    }
+
+    #if canImport(UIKit)
+    func testAddSubview() {
+
+        let view = UIView()
+        let subView = UIView()
+
+        view.addSubview(subView)
+        XCTAssertTrue(view.subviews.contains(subView))
+        XCTAssertTrue(subView.superview == view)
+    }
+    #endif
 }
